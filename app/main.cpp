663423c8afdcc3c9b5f851f8f70b0a93ba93e21d
vtoledo27 <83180710+vtoledo27@users.noreply.github.com>
2022-09-03 17:52:54 -0400

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -17,17 +17,18 @@ void participantes();
 * @param argv argumentos
 * @return El código de salida del programa
 */
-int main(int argc, char** argv) {
-	
-    int codSalida;
+
+int Salida(int argc, char** argv)
+{
+	int codSalida;
 	if (argc == 2) {
 		const string polinomio(argv[1]);
-		vector<Monomio> monomios = Utils::convertir(polinomio);
+		vector<Monomio> monomios = Utils::convertir(polinomio); //Se llama a la funcion convertir y se ingresa el vector retornado a la variable "monomios"
 
 		//Se muestra el polinomio Ingresado por el usuario
-	    cout<<"-Polinomio: ";
+	    cout<<"-Polinomio (f(x)): ";
 		vector<Monomio>::size_type i = 0;
-		//Mientras el tamaño del monomio sea mayor a "i"
+		//Mientras el tamaño del vector monomio sea mayor a "i"
 		for(i=0; i< monomios.size(); i++){
 			Monomio mono = monomios[i];
 			cout<<mono.GetCoeficiente()<<"x**"<<mono.GetGrado()<<" ";
@@ -36,24 +37,28 @@ int main(int argc, char** argv) {
 
 		cout<<"-Solucion(aproximada): "<<resultado<<endl;
 		codSalida = EXIT_SUCCESS;
-	    } else {//En caso de que el usuario ingrese erroneamente o en el formato incorrecto los argumentos se muestra el siguiente mensaje
+	    } else {//En caso de que el usuario ingrese erroneamente o en el formato incorrecto los argumentos se mostrara el siguiente mensaje
 			cout<<"Error: argumentos no validos, deben ingresarse en el formato: "<<endl;
 			cout<<argv[0] << "''->_Polinomio_<-''" <<endl;
 			cout<<endl;
 			cout<<"Porfavor intentelo nuevamente"<<endl;
 			codSalida = EXIT_FAILURE;
-
-
 	    }
 	    //Funcion que muestra en pantalla a los integrantes del grupo
 	    participantes();
 	 return codSalida;
-	}
+}
 
 
 
+int main(int argc, char** argv){
 
-	/*Funcion que muestra a los integrantes del grupo*/
+	Salida(argc, argv);
+}
+
+
+
+/*Funcion que muestra a los integrantes del grupo*/
 void participantes() {
    cout<<endl<< "-|=== Taller N°1 ===|-" <<endl;
    cout<<endl << "-Kevin Labra Soto";
@@ -61,8 +66,5 @@ void participantes() {
    cout<<endl << "-Virla Parra Retamal" <<endl;
    cout<<endl;
    cout<<endl;
-
 	}
 
-	
-	
